c0f638b48f0aa422fe473745f14a0cda40127e86
axe-e <102362908+axe-e@users.noreply.github.com>
2022-04-05 23:02:45 +0530

    Update Build.sh

--- Build.sh
+++ Build.sh
@@ -1,5 +1,9 @@
 git clone https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp.git -b twrp-11
 git clone https://github.com/shashankx86/android_device_xiaomi_spes-TWRP.git -b twrp-11 device/xiaomi/spes
+pwd
+ls
+cd build ls
+ls 
 source build/envsetup.sh
 lunch twrp_spes-eng
 mka bootimage
