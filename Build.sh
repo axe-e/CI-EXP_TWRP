36ad4de55b43275aca12f20738e6d2863a058a03
axe-e <102362908+axe-e@users.noreply.github.com>
2022-04-05 23:20:46 +0530

    Update Build.sh

--- Build.sh
+++ Build.sh
@@ -1,6 +1,7 @@
-git clone https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp.git -b twrp-11
-cd platform_manifest_twrp_aosp
+repo init --depth=1 -u https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp.git -b twrp-11
 git clone https://github.com/shashankx86/android_device_xiaomi_spes-TWRP.git -b twrp-11 device/xiaomi/spes
+
+export ALLOW_MISSING_DEPENDENCIES=true
 pwd
 ls
 source build/envsetup.sh
